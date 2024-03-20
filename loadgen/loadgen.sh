5f6f996544605d542ed62bd0d5a23d89e538c9bf
vic-lsh <shli@cs.washington.edu>
2024-03-20 15:19:26 -0700

    update loadgen script with timeout

--- loadgen/loadgen.sh
+++ loadgen/loadgen.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-set -e
+# set -e
 
-# first, sanity check to make sure the service is up and port is exposed
-curl localhost:8080
+URL=localhost:8080
+HTTP_URL=http://$URL
+DUR_SECS=5
 
-locust --headless --users 100 --spawn-rate 1 -H http://localhost:8080
+healthcheck() {
+    echo "first, a healthcheck..."
+    # first, sanity check to make sure the service is up and port is exposed
+    curl $URL >/dev/null 2>&1 || { echo "healthcheck failed, make sure endpoint is up"; exit 1; }
+}
+
+timed_loadgen() {
+    locust -t ${DUR_SECS}s --headless --users 100 --spawn-rate 1 -H $HTTP_URL
+}
+
+healthcheck && timed_loadgen
+
+echo "finished loadgen for $DUR_SECS seconds!"
