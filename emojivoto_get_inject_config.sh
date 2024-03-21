05e53d527bfe5c27aaa95345f9dbb859d801f834
vic-lsh <shli@cs.washington.edu>
2024-03-20 18:32:37 -0700

    rm legacy script

--- emojivoto_get_inject_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
- kubectl get -n emojivoto deploy -o yaml  \
-     | linkerd inject --proxy-image docker.io/vicsli/lkd-proxy-dev25 --manual - > test.yaml
-
- # kubectl get -n emojivoto deploy -o json  \
- #     | linkerd inject --proxy-image docker.io/vicsli/lkd-proxy-dev25 --manual - > test.json
