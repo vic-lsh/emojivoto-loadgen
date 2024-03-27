98026c576d533e933df78b4c940806b89db0726d
vic-lsh <shli@cs.washington.edu>
2024-03-27 12:04:36 -0700

    apply service profile before running expr

--- run_expr.sh
+++ run_expr.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+apply_service_profiles() {
+    echo "applying profiles.."
+    kubectl apply -f ./emojivoto/training/service-profiles/emoji-svc-profile.yml
+    kubectl apply -f ./emojivoto/training/service-profiles/voting-svc-profile.yml
+    kubectl apply -f ./emojivoto/training/service-profiles/web-service-profile-with-timeout.yml
+    # kubectl apply -f ./emojivoto/training/service-profiles/web-service-profile-with-retry-budget.yml
+
+    echo "sleep to wait for profiles to take effect"
+    sleep 10
+}
+
+apply_service_profiles
+
 # loadgen script assumes its pwd is its directory.
 cd loadgen
 ./loadgen.sh
